59e8bf46ba408b98aa48abe65f4b77f6fee501bd
The Typhothanian <thetyphothanian@gmail.com>
2026-09-04 07:28:28 -0600

    stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,52 @@
+import java.nio.file.Paths
+
 plugins {
     kotlin("jvm") version "2.4.0"
+    `java-gradle-plugin`
+    `maven-publish`
 }
 
 group = "net.typho"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
+    maven("https://typho.net/maven")
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 kotlin {
-    jvmToolchain(25)
+    jvmToolchain(8)
 }
 
-tasks.test {
-    useJUnitPlatform()
+dependencies {
+}
+
+gradlePlugin {
+    plugins {
+        create("typho_publish") {
+            id = "net.typho.typho_publish"
+            implementationClass = "net.typho.typho_publish.TyphoPublishPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "typho"
+            url = Paths.get(project.findProperty("typho_publish.website_dir") as String).resolve("maven").toUri()
+        }
+    }
 }
--- src/main/kotlin/net/typho/typho_publish/TyphoPublishPlugin.kt
+++ src/main/kotlin/net/typho/typho_publish/TyphoPublishPlugin.kt
@@ -1,4 +1,46 @@
 package net.typho.typho_publish
 
-class TyphoPublishPlugin {
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import java.io.File
+
+class TyphoPublishPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        project.findProperty("typho_publish.website_dir")?.toString()?.let { websiteDir ->
+            val websiteDir = File(websiteDir)
+
+            project.plugins.apply("maven-publish")
+
+            val publishing = project.extensions.getByType(PublishingExtension::class.java)
+            val publication = publishing.publications.create("maven", MavenPublication::class.java)
+            publication.from(project.components.getByName("java"))
+            publishing.repositories.maven {
+                it.name = "typho"
+                it.url = websiteDir.resolve("maven").toURI()
+            }
+
+            val commitTask = project.tasks.register("commitAndPushTyphoRepository") {
+                it.group = "publishing"
+                it.doLast {
+                    fun run(vararg args: String) {
+                        val exit = ProcessBuilder(*args).directory(websiteDir).inheritIO().start().waitFor()
+
+                        if (exit != 0) {
+                            throw RuntimeException("Error running command \"${args.joinToString(separator = " ")}\", exit code $exit")
+                        }
+                    }
+
+                    run("git", "add", "maven/")
+                    run("git", "commit", "-m", "Published ${publication.groupId}:${publication.artifactId}:${publication.version}")
+                    run("git", "push")
+                }
+            }
+
+            project.tasks.getByName("publishMavenPublicationToTyphoRepository") {
+                it.finalizedBy(commitTask)
+            }
+        }
+    }
 }
